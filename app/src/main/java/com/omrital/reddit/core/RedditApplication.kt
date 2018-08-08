007186acaee9a6171e40b224be268849f0cff680
Omri Tal <>
2018-08-08 18:10:15 +0300

    fix realm crash & infalting crash

--- app/src/main/java/com/omrital/reddit/core/RedditApplication.kt
+++ app/src/main/java/com/omrital/reddit/core/RedditApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import com.omrital.reddit.dagger.AppComponent
 import com.omrital.reddit.dagger.AppModule
 import com.omrital.reddit.dagger.DaggerAppComponent
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.realm.rx.RealmObservableFactory
 
 class RedditApplication: Application() {
 
@@ -12,10 +15,19 @@ class RedditApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         this.appComponent = initDagger(this)
+        initRealm()
     }
 
     private fun initDagger(app: RedditApplication): AppComponent =
             DaggerAppComponent.builder()
                     .appModule(AppModule(app))
                     .build()
+
+    private fun initRealm() {
+        Realm.init(this)
+        val config = RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        Realm.setDefaultConfiguration(config)
+    }
 }
